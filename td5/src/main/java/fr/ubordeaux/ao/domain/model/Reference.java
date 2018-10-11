1a7233f8bdd9794182f575bdc4cdc217c19c19b6
michelmassamiri <mmassamire@emi.u-bordeaux1.fr>
2018-10-11 09:00:51 +0200

    fixing code style

--- td5/src/main/java/fr/ubordeaux/ao/domain/model/Reference.java
+++ td5/src/main/java/fr/ubordeaux/ao/domain/model/Reference.java
@@ -11,7 +11,8 @@ public class Reference {
     private String description;
     private Price basePrice;
 
-    public Reference(String id, String name, String description, Price basePrice) {
+    public Reference(String id, String name, String description,
+    Price basePrice) {
         this.setReferenceId(id);
         this.setName(name);
         this.setDescription(description);
@@ -19,22 +20,34 @@ public class Reference {
     }
 
     private void setReferenceId(String id) {
-        if (id == null) throw new ReferenceManagementException("cannot create reference with null id");
+        if (id == null) {
+        	throw new ReferenceManagementException(
+    			"cannot create reference with null id");
+        }
         this.id = id;
     }
 
     private void setName(String name) {
-        if (name == null) throw new ReferenceManagementException("cannot create reference with null name");
+        if (name == null) {
+        	throw new ReferenceManagementException(
+    			"cannot create reference with null name");
+        }
         this.name = name;
     }
 
     private void setDescription(String description) {
-        if (description == null) throw new ReferenceManagementException("cannot create reference with null description");
+        if (description == null) {
+        	throw new ReferenceManagementException(
+    			"cannot create reference with null description");
+        }
         this.description = description;
     }
 
     private void setBasePrice(Price basePrice) {
-        if (basePrice == null) throw new ReferenceManagementException("cannot create reference with null basePrice");
+        if (basePrice == null) {
+        	throw new ReferenceManagementException(
+    			"cannot create reference with null basePrice");
+        }
         this.basePrice = basePrice;
     }
 
@@ -57,8 +70,9 @@ public class Reference {
     @Override
     public boolean equals(Object other) {
         if (other instanceof Reference) {
-            Reference otherReference = (Reference)other;
-            boolean equals = this.getId().compareTo(otherReference.getId())==0;
+            Reference otherReference = (Reference) other;
+            boolean equals = this.getId()
+        		.compareTo(otherReference.getId()) == 0;
 			return equals;
         } else {
             return false;
@@ -72,6 +86,9 @@ public class Reference {
 
     @Override
     public String toString() {
-        return "Reference id="+id+ " name="+name+" description="+description;
+        return "Reference id=" + id
+        		+ " name=" + name
+        		+ " description="
+        		+ description;
     }
 }
